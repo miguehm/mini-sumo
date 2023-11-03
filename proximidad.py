0cfd652ad1cb3790d695a18938b3599d7e401bab
miguehm <miguel.hernandezmor@alumno.buap.mx>
2023-11-03 16:05:30 -0600

    add: proximity functions

---
+++ proximidad.py
@@ -0,0 +1,44 @@
+from machine import Pin
+import time
+
+trig = Pin(17, Pin.OUT)
+echo = Pin(16, Pin.IN, Pin.PULL_DOWN)
+
+# Has no time sleep
+def get_distance():
+    trig.value(0)
+    time.sleep(0.1)
+    trig.value(1)
+    time.sleep_us(2)
+    trig.value(0)
+    while echo.value()==0:
+      pulse_start = time.ticks_us()
+    while echo.value()==1:
+      pulse_end = time.ticks_us()
+    pulse_duration = pulse_end - pulse_start
+    distance = pulse_duration * 17165 / 1000000
+    distance = round(distance, 0)
+    print ('Distance:',"{:.0f}".format(distance),'cm')
+
+def testing():
+    while True:
+        trig.value(0)
+        time.sleep(0.1)
+        trig.value(1)
+        time.sleep_us(2)
+        trig.value(0)
+        while echo.value()==0:
+          pulse_start = time.ticks_us()
+        while echo.value()==1:
+          pulse_end = time.ticks_us()
+        pulse_duration = pulse_end - pulse_start
+        distance = pulse_duration * 17165 / 1000000
+        distance = round(distance, 0)
+        print ('Distance:',"{:.0f}".format(distance),'cm')
+        time.sleep(1)
+
+if __name__ == "__main__":
+    while True:
+        get_distance()
+        time.sleep(1/100)
+    
